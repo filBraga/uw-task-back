9db43e8936a8f337ddeba35b3bb74db22da06109
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-19 17:47:30 -0300

    feat: cinema entity

--- src/modules/cinema/entities/cinema.entity.ts
+++ src/modules/cinema/entities/cinema.entity.ts
@@ -1,1 +1,33 @@
-export class Cinema {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Hall } from '../../hall/entities/hall.entity';
+
+@Entity('cinema')
+export class Cinema {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Hall, (hall) => hall.cinema)
+  @JoinColumn({ name: 'hallId' })
+  halls: Hall[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
