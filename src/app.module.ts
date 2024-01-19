35fd56d5fc05d0bf20f802b66757fca0d0e69ab5
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-19 12:56:32 -0300

    feat: adding .env in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CinemaModule } from './modules/cinema/cinema.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // do not use in prod
+    }),
+    CinemaModule,
+  ],
 })
 export class AppModule {}
