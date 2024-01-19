ec54924cffb1598eea93871ee028d2268bf03843
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-19 12:57:19 -0300

    feat: adding data source

---
+++ src/typeorm/database/data-source.ts
@@ -0,0 +1,15 @@
+// data-source.ts
+
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: 'password',
+  database: 'postgres',
+  entities: ['src/**/*.entity{.js,.ts}'],
+};
+
+export default new DataSource(dataSourceOptions);
