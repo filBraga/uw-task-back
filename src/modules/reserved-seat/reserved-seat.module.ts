fc9c3a9452920f06bc66dbbd114c9dd614aed92f
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-19 18:23:08 -0300

    feat: adding dependencies

--- src/modules/reserved-seat/reserved-seat.module.ts
+++ src/modules/reserved-seat/reserved-seat.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReservedSeatService } from './reserved-seat.service';
 import { ReservedSeatController } from './reserved-seat.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReservedSeats } from './entities/reserved-seat.entity';
 
 @Module({
   controllers: [ReservedSeatController],
   providers: [ReservedSeatService],
+  imports: [TypeOrmModule.forFeature([ReservedSeats])],
 })
 export class ReservedSeatModule {}
