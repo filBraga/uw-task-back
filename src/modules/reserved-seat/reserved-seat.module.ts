fa31594523da4781827f42f831b9782328989cd4
Filipe Braga <filipe.b.braga@gmail.com>
2024-01-19 23:09:08 -0300

    feat: adding hall entity to modules

--- src/modules/reserved-seat/reserved-seat.module.ts
+++ src/modules/reserved-seat/reserved-seat.module.ts
@@ -3,10 +3,14 @@ import { ReservedSeatService } from './reserved-seat.service';
 import { ReservedSeatController } from './reserved-seat.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservedSeats } from './entities/reserved-seat.entity';
+import { Hall } from '../hall/entities/hall.entity';
 
 @Module({
   controllers: [ReservedSeatController],
   providers: [ReservedSeatService],
-  imports: [TypeOrmModule.forFeature([ReservedSeats])],
+  imports: [
+    TypeOrmModule.forFeature([ReservedSeats]),
+    TypeOrmModule.forFeature([Hall]),
+  ],
 })
 export class ReservedSeatModule {}
